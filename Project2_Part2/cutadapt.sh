2d6d1d67e85933a94bb03facb6a2c286c698ecfd
Cheryl <cherylsung@gmail.com>
2026-09-02 17:52:08 -0700

    reuploading cutadapt.sh without the first part commented out

--- Project2_Part2/cutadapt.sh
+++ Project2_Part2/cutadapt.sh
@@ -6,12 +6,12 @@
 #SBATCH --job-name=cutadapt
 #SBATCH --time=12:00:00
 
-# /usr/bin/time -v cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
-# 	-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
-# 	-o Cco_com101_EO_adult_1_1_cut.fastq.gz \
-# 	-p Cco_com101_EO_adult_1_2_cut.fastq.gz \
-# 	Cco_com101_EO_adult_1_1.fastq.gz \
-# 	Cco_com101_EO_adult_1_2.fastq.gz
+/usr/bin/time -v cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
+	-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
+	-o Cco_com101_EO_adult_1_1_cut.fastq.gz \
+	-p Cco_com101_EO_adult_1_2_cut.fastq.gz \
+	Cco_com101_EO_adult_1_1.fastq.gz \
+	Cco_com101_EO_adult_1_2.fastq.gz
 
 /usr/bin/time -v cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
 	-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
